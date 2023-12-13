acd48f3a440a985acc9c1d468e83ba3d3f1a88fe
RiccardoPollice <riccardo.pollice17@gmail.com>
2023-12-13 16:35:28 +0100

    Working

--- src/org/lessons/java/valutazioni/Corso.java
+++ src/org/lessons/java/valutazioni/Corso.java
@@ -1,2 +1,51 @@
-package org.lessons.java.valutazioni;public class Corso {
+package org.lessons.java.valutazioni;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class Corso {
+    private ArrayList<Studente>studentiTot = new ArrayList<>();
+
+    public void getStudents(ArrayList<Studente>studentiTot){
+        for (Studente studente: studentiTot){
+            System.out.println(studente);
+        }
+
+
+    }
+    public void removeStudent(ArrayList<Studente>studentiTot,int ID){
+        studentiTot.remove(ID);
+
+    }
+    public void addStudent(ArrayList<Studente>studentiTot,Studente nuovoStudente){
+        studentiTot.remove(nuovoStudente);
+
+    }
+    public  void getPercStudentiPromossi (ArrayList<Studente>studentiTot){
+        ArrayList<Studente>promossi= new ArrayList<>();
+        for (Studente studente:studentiTot){
+            if (studente.getStudentResult()){
+                promossi.add(studente);
+
+            }
+
+
+        }
+        double perPromossi =(promossi.size()*100.0/studentiTot.size());
+        DecimalFormat df = new DecimalFormat("#.##");
+        String perPromossiFormatted = df.format(perPromossi);
+        System.out.println("la percentuale di studenti promossi è:"+perPromossiFormatted + %);
+        System.out.println("ecco gli studenti promossi: ");
+        for (Studente studentepromosso : promossi){
+            System.out.println(studentepromosso);
+        }
+
+    }
+
+
+
+
+
+
+}
 }
--- src/org/lessons/java/valutazioni/Studente.java
+++ src/org/lessons/java/valutazioni/Studente.java
@@ -1,2 +1,59 @@
-package org.lessons.java.valutazioni;public class Studente {
+package org.lessons.java.valutazioni;
+
+public class Studente {
+    private int id;
+    private static int counter = 1;
+    private int absencesPer;
+    private double averageGrades;
+
+
+    public Studente (int absencesPer, double averageGrades) {
+        this.id = counter++;
+        this.absencesPer = absencesPer;
+        this.averageGrades = averageGrades;
+    }
+
+    public final int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAbsencesPer() {
+        return absencesPer;
+    }
+
+    public void setAbsencesPer(int absencesPer) {
+        this.absencesPer = absencesPer;
+    }
+
+    public double getAverageGrades() {
+        return averageGrades;
+    }
+
+    public void setAverageGrades(double averageGrades) {
+        this.averageGrades = averageGrades;
+    }
+    public boolean getStudentResult(){
+        if (absencesPer>50){
+            return false;
+        }
+        if (absencesPer>25 && absencesPer<50 && averageGrades<=2){
+            return false;
+        }
+        if (absencesPer<25 && averageGrades<2){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    @Override
+    public String toString() {
+        return "id:"+id+ " percentuale assenze= "+ absencesPer+" %"+ " media voti: "+averageGrades;
+    }
+
+
 }
